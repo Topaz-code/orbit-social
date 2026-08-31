90f2010202c99b9eb3a16c41f84bd2bd8e971513
Topaz-Code <erabo65@gmail.com>
2026-08-31 21:22:15 +0100

    fix(mobile): add metro.config, babel.config, styles.xml, drawable icon, and simplify MainApplication

--- mobile/android/app/src/main/java/com/orbit/social/MainApplication.kt
+++ mobile/android/app/src/main/java/com/orbit/social/MainApplication.kt
@@ -3,13 +3,9 @@ package com.orbit.social
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -23,18 +19,12 @@ class MainApplication : Application(), ReactApplication {
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        override val isNewArchEnabled: Boolean = false
+        override val isHermesEnabled: Boolean = true
       }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      load()
-    }
+    SoLoader.init(this, false)
   }
 }
