5c9d91c00e76213efeea76dd640f064528039718
Topaz-Code <erabo65@gmail.com>
2026-08-31 21:28:59 +0100

    fix(mobile): add proguard-rules.pro, update build-apk.yml trigger and logging, and refine MainActivity

---
+++ mobile/android/app/proguard-rules.pro
@@ -0,0 +1,4 @@
+# Proguard rules for Orbit Mobile
+-keep class com.facebook.react.** { *; }
+-keep class com.orbit.social.** { *; }
+-dontwarn com.facebook.react.**
--- mobile/android/app/src/main/java/com/orbit/social/MainActivity.kt
+++ mobile/android/app/src/main/java/com/orbit/social/MainActivity.kt
@@ -2,12 +2,11 @@ package com.orbit.social
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "orbit-mobile"
 
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      DefaultReactActivityDelegate(this, mainComponentName, false)
 }
